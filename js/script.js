64f245762b18534c5185413a9092e0cb6c447e5d
Oliver <oliver.mwangale@moringaschool.com>
2021-11-22 21:11:23 +0300

    add the toggle functionality in script file

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+
+$(document).ready(function(){
+    $("#icon1").click(function(){
+        $("#content1").toggle();
+        $("#img1").toggle();
+    })
+
+    $("#icon2").click(function(){
+        $("#content2").toggle();
+        $("#img2").toggle();
+    })
+
+    $("#icon3").click(function(){
+        $("#content3").toggle();
+        $("#img3").toggle();
+    })
+})
+
+
+
+
+
