4535aabbe4fb08e6d8c365e819b6978613f95f1c
Oliver <oliver.mwangale@moringaschool.com>
2021-11-22 22:53:48 +0300

    updates js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,43 @@
+// Business Logic
+var data = 
+
+
+//Userinter face
+$(document).ready(function(){
+    $( ".hover1" ).hover(function() {
+        $(".txt1").toggle();
+      });
+      
+    $( ".hover2" ).hover(function() {
+        $(".txt2").toggle();
+      });
+      
+    $( ".hover3" ).hover(function() {
+        $(".txt3").toggle();
+      });
+      
+    $( ".hover4" ).hover(function() {
+        $(".txt4").toggle();
+      });
+      
+    $( ".hover5" ).hover(function() {
+        $(".txt5").toggle();
+      });
+      
+    $( ".hover6" ).hover(function() {
+        $(".txt6").toggle();
+      });
+      
+    $( ".hover7" ).hover(function() {
+        $(".txt7").toggle();
+      });
+      
+    $( ".hover8" ).hover(function() {
+        $(".txt8").toggle();
+      });
+      
+});
+
 
 
 $(document).ready(function(){
@@ -16,7 +56,6 @@ $(document).ready(function(){
         $("#img3").toggle();
     })
 })
-
 
 
 
